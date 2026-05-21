60ec796f5148df106885159257c84732072c0b49
akeinb <akeinbantin@gmail.com>
2026-05-20 21:12:04 -0500

    Add project files.

---
+++ TikTocToe_V1/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace TikTocToe_V1
+{
+    partial class TikTokToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.BTN_Section_9 = new System.Windows.Forms.Button();
+            this.BTN_Section_8 = new System.Windows.Forms.Button();
+            this.BTN_Section_7 = new System.Windows.Forms.Button();
+            this.BTN_Section_6 = new System.Windows.Forms.Button();
+            this.BTN_Section_5 = new System.Windows.Forms.Button();
+            this.BTN_Section_4 = new System.Windows.Forms.Button();
+            this.BTN_Section_3 = new System.Windows.Forms.Button();
+            this.BTN_Section_2 = new System.Windows.Forms.Button();
+            this.BTN_Section_1 = new System.Windows.Forms.Button();
+            this.LB_TikTokToe = new System.Windows.Forms.Label();
+            this.BTN_ClearBoard = new System.Windows.Forms.Button();
+            this.LB_Player1 = new System.Windows.Forms.Label();
+            this.LB_Player2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.BTN_Section_9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.BTN_Section_8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.BTN_Section_7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.BTN_Section_6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.BTN_Section_5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.BTN_Section_4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.BTN_Section_3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.BTN_Section_2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.BTN_Section_1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(44, 154);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(498, 491);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // BTN_Section_9
+            // 
+            this.BTN_Section_9.Location = new System.Drawing.Point(335, 329);
+            this.BTN_Section_9.Name = "BTN_Section_9";
+            this.BTN_Section_9.Size = new System.Drawing.Size(160, 159);
+            this.BTN_Section_9.TabIndex = 8;
+            this.BTN_Section_9.UseVisualStyleBackColor = true;
+            // 
+            // BTN_Section_8
+            // 
+            this.BTN_Section_8.Location = new System.Drawing.Point(169, 329);
+            this.BTN_Section_8.Name = "BTN_Section_8";
+            this.BTN_Section_8.Size = new System.Drawing.Size(160, 159);
+            this.BTN_Section_8.TabIndex = 7;
+            this.BTN_Section_8.UseVisualStyleBackColor = true;
+            // 
+            // BTN_Section_7
+            // 
+            this.BTN_Section_7.Location = new System.Drawing.Point(3, 329);
+            this.BTN_Section_7.Name = "BTN_Section_7";
+            this.BTN_Section_7.Size = new System.Drawing.Size(160, 159);
+            this.BTN_Section_7.TabIndex = 6;
+            this.BTN_Section_7.UseVisualStyleBackColor = true;
+            // 
+            // BTN_Section_6
+            // 
+            this.BTN_Section_6.Location = new System.Drawing.Point(335, 166);
+            this.BTN_Section_6.Name = "BTN_Section_6";
+            this.BTN_Section_6.Size = new System.Drawing.Size(160, 157);
+            this.BTN_Section_6.TabIndex = 5;
+            this.BTN_Section_6.UseVisualStyleBackColor = true;
+            // 
+            // BTN_Section_5
+            // 
+            this.BTN_Section_5.Location = new System.Drawing.Point(169, 166);
+            this.BTN_Section_5.Name = "BTN_Section_5";
+            this.BTN_Section_5.Size = new System.Drawing.Size(160, 157);
+            this.BTN_Section_5.TabIndex = 4;
+            this.BTN_Section_5.UseVisualStyleBackColor = true;
+            // 
+            // BTN_Section_4
+            // 
+            this.BTN_Section_4.Location = new System.Drawing.Point(3, 166);
+            this.BTN_Section_4.Name = "BTN_Section_4";
+            this.BTN_Section_4.Size = new System.Drawing.Size(160, 157);
+            this.BTN_Section_4.TabIndex = 3;
+            this.BTN_Section_4.UseVisualStyleBackColor = true;
+            // 
+            // BTN_Section_3
+            // 
+            this.BTN_Section_3.Location = new System.Drawing.Point(335, 3);
+            this.BTN_Section_3.Name = "BTN_Section_3";
+            this.BTN_Section_3.Size = new System.Drawing.Size(160, 157);
+            this.BTN_Section_3.TabIndex = 2;
+            this.BTN_Section_3.UseVisualStyleBackColor = true;
+            // 
+            // BTN_Section_2
+            // 
+            this.BTN_Section_2.Location = new System.Drawing.Point(169, 3);
+            this.BTN_Section_2.Name = "BTN_Section_2";
+            this.BTN_Section_2.Size = new System.Drawing.Size(160, 157);
+            this.BTN_Section_2.TabIndex = 1;
+            this.BTN_Section_2.UseVisualStyleBackColor = true;
+            // 
+            // BTN_Section_1
+            // 
+            this.BTN_Section_1.Location = new System.Drawing.Point(3, 3);
+            this.BTN_Section_1.Name = "BTN_Section_1";
+            this.BTN_Section_1.Size = new System.Drawing.Size(160, 157);
+            this.BTN_Section_1.TabIndex = 0;
+            this.BTN_Section_1.UseVisualStyleBackColor = true;
+            // 
+            // LB_TikTokToe
+            // 
+            this.LB_TikTokToe.AutoSize = true;
+            this.LB_TikTokToe.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_TikTokToe.Location = new System.Drawing.Point(211, 25);
+            this.LB_TikTokToe.Name = "LB_TikTokToe";
+            this.LB_TikTokToe.Size = new System.Drawing.Size(183, 31);
+            this.LB_TikTokToe.TabIndex = 1;
+            this.LB_TikTokToe.Text = "TIK TOK TOE";
+            // 
+            // BTN_ClearBoard
+            // 
+            this.BTN_ClearBoard.Location = new System.Drawing.Point(217, 701);
+            this.BTN_ClearBoard.Name = "BTN_ClearBoard";
+            this.BTN_ClearBoard.Size = new System.Drawing.Size(139, 36);
+            this.BTN_ClearBoard.TabIndex = 2;
+            this.BTN_ClearBoard.Text = "Clear Board";
+            this.BTN_ClearBoard.UseVisualStyleBackColor = true;
+            // 
+            // LB_Player1
+            // 
+            this.LB_Player1.AutoSize = true;
+            this.LB_Player1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_Player1.Location = new System.Drawing.Point(55, 22);
+            this.LB_Player1.Name = "LB_Player1";
+            this.LB_Player1.Size = new System.Drawing.Size(65, 20);
+            this.LB_Player1.TabIndex = 3;
+            this.LB_Player1.Text = "Player 1";
+            // 
+            // LB_Player2
+            // 
+            this.LB_Player2.AutoSize = true;
+            this.LB_Player2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_Player2.Location = new System.Drawing.Point(375, 22);
+            this.LB_Player2.Name = "LB_Player2";
+            this.LB_Player2.Size = new System.Drawing.Size(65, 20);
+            this.LB_Player2.TabIndex = 4;
+            this.LB_Player2.Text = "Player 2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.LB_Player2);
+            this.groupBox1.Controls.Add(this.LB_Player1);
+            this.groupBox1.Location = new System.Drawing.Point(47, 82);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(494, 62);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // TikTokToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 770);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.BTN_ClearBoard);
+            this.Controls.Add(this.LB_TikTokToe);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "TikTokToe";
+            this.Text = "TikTokToe";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button BTN_Section_9;
+        private System.Windows.Forms.Button BTN_Section_8;
+        private System.Windows.Forms.Button BTN_Section_7;
+        private System.Windows.Forms.Button BTN_Section_6;
+        private System.Windows.Forms.Button BTN_Section_5;
+        private System.Windows.Forms.Button BTN_Section_4;
+        private System.Windows.Forms.Button BTN_Section_3;
+        private System.Windows.Forms.Button BTN_Section_2;
+        private System.Windows.Forms.Button BTN_Section_1;
+        private System.Windows.Forms.Label LB_TikTokToe;
+        private System.Windows.Forms.Button BTN_ClearBoard;
+        private System.Windows.Forms.Label LB_Player1;
+        private System.Windows.Forms.Label LB_Player2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
