f231723c14299929328f29619cf8b2c88e761b49
akeinb <akeinbantin@gmail.com>
2026-05-27 12:13:43 -0500

    Sound effects added -selection sound effects -invalid selection sound effects -background music (stops working when playing)

--- TikTocToe_V1/TikTocToe.cs
+++ TikTocToe_V1/TikTocToe.cs
@@ -9,17 +9,29 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace TikTocToe_V1
 {
     public partial class TikTocToe : Form
     {
-        public string _Player1 = "X";
-        public string _Player2 = "O";
-        public string _CurrentPlayer = null;
-        public bool win = false;
+        private string _Player1;
+        private string _Player2;
+        private string _CurrentPlayer;
+        private bool win;
+        private SoundPlayer selectSound;
+        private SoundPlayer wrongSelectionSound;
+        private SoundPlayer BgMusic;
         public TikTocToe()
         {
-            InitializeComponent();  
+            InitializeComponent();
+            _Player1 = "X";
+            _Player2 = "O";
+            _CurrentPlayer = null;
+            win = false;
+            selectSound = new SoundPlayer("selectionbeep.wav");
+            wrongSelectionSound = new SoundPlayer("wrongselectionbeep.wav");
+            BgMusic = new SoundPlayer("BG Music.wav");
+            BgMusic.PlayLooping();
         }
 
         private void TikTokToe_Load(object sender, EventArgs e)
@@ -36,12 +48,14 @@ namespace TikTocToe_V1
             BTN_Section_7.Text = " ";
             BTN_Section_8.Text = " ";
             BTN_Section_9.Text = " ";
+            
         }
 
         private void BTN_Section_1_Click(object sender, EventArgs e)
         {
             if(BTN_Section_1.Text == " ")
             {
+                selectSound.Play();
                 BTN_Section_1.Text = _CurrentPlayer;
                
                 CheckWin();
@@ -49,7 +63,10 @@ namespace TikTocToe_V1
 
                 _CurrentPlayer = PlayerTurn(_CurrentPlayer);
             }
-            
+            else
+            {
+                wrongSelectionSound.Play();
+            }
             
         }
 
@@ -57,23 +74,33 @@ namespace TikTocToe_V1
         {
             if (BTN_Section_2.Text == " ")
             {
+                selectSound.Play();
                 BTN_Section_2.Text = _CurrentPlayer;
                 CheckWin();
                 checkDraw();
                 _CurrentPlayer = PlayerTurn(_CurrentPlayer);
             }
-              
+            else
+            {
+                wrongSelectionSound.Play();
+            }
+
         }
 
         private void BTN_Section_3_Click(object sender, EventArgs e)
         {
             if (BTN_Section_3.Text == " ")
             {
+                selectSound.Play();
                 BTN_Section_3.Text = _CurrentPlayer;
                
                 CheckWin();
                 checkDraw();
                 _CurrentPlayer = PlayerTurn(_CurrentPlayer);
+            }
+            else
+            {
+                wrongSelectionSound.Play();
             }
         }
 
@@ -81,78 +108,104 @@ namespace TikTocToe_V1
         {
             if (BTN_Section_4.Text == " ")
             {
+                selectSound.Play();
                 BTN_Section_4.Text = _CurrentPlayer;
                 
                 CheckWin();
                 checkDraw();
                 _CurrentPlayer = PlayerTurn(_CurrentPlayer);
             }
-               
+            else
+            {
+                wrongSelectionSound.Play();
+            }
+
         }
 
         private void BTN_Section_5_Click(object sender, EventArgs e)
         {
             if (BTN_Section_5.Text == " ")
             {
+                selectSound.Play();
                 BTN_Section_5.Text = _CurrentPlayer;
                 
                 CheckWin();
                 checkDraw();
                 _CurrentPlayer = PlayerTurn(_CurrentPlayer);
             }
-               
+            else
+            {
+                wrongSelectionSound.Play();
+            }
         }
 
         private void BTN_Section_6_Click(object sender, EventArgs e)
         {
             if (BTN_Section_6.Text == " ")
             {
+                selectSound.Play();
                 BTN_Section_6.Text = _CurrentPlayer;
                 
                 CheckWin();
                 checkDraw();
                 _CurrentPlayer = PlayerTurn(_CurrentPlayer);
             }
-               
+            else
+            {
+                wrongSelectionSound.Play();
+            }
+
         }
 
         private void BTN_Section_7_Click(object sender, EventArgs e)
         {
             if (BTN_Section_7.Text == " ")
             {
+                selectSound.Play();
                 BTN_Section_7.Text = _CurrentPlayer;
                 
                 CheckWin();
                 checkDraw();
                 _CurrentPlayer = PlayerTurn(_CurrentPlayer);
             }
-               
+            else
+            {
+                wrongSelectionSound.Play();
+            }
         }
 
         private void BTN_Section_8_Click(object sender, EventArgs e)
         {
             if (BTN_Section_8.Text == " ")
             {
+                selectSound.Play();
                 BTN_Section_8.Text = _CurrentPlayer;
                 
                 CheckWin();
                 checkDraw();
                 _CurrentPlayer = PlayerTurn(_CurrentPlayer);
             }
-               
+            else
+            {
+                wrongSelectionSound.Play();
+            }
         }
 
         private void BTN_Section_9_Click(object sender, EventArgs e)
         {
             if (BTN_Section_9.Text == " ")
             {
+                selectSound.Play();
                 BTN_Section_9.Text = _CurrentPlayer;
                 
                 CheckWin();
                 checkDraw();
                 _CurrentPlayer = PlayerTurn(_CurrentPlayer);
             }
-               
+            else
+            {
+                wrongSelectionSound.Play();
+            }
         }
 
         private void BTN_ClearBoard_Click(object sender, EventArgs e)
